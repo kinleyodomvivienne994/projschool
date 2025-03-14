7e61e3a121727e455b19bab81899e0360f22c917
kinleyodomvivienne994 <kinleyodomvivienne994@gmail.com>
2025-03-14 23:00:23 +0000

    Add 9.py

---
+++ src/9.py
@@ -0,0 +1,6 @@
+import random
+def random_string(length):
+    letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    return ''.join(random.choice(letters) for i in range(length))
+
+print(random_string(10))
